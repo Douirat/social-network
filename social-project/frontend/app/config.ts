2fe4968e3d9ddeb40c1be4dfb14f4fe29fcdc46f
Bennacer Douirat <bennacer.douirat.solicode@gmail.com>
2025-07-11 23:51:44 +0100

    Removed old social-project folder

--- social-project/frontend/app/config.ts
+++
@@ -1,5 +0,0 @@
-export const API_BASE_URL = 'http://localhost:8080'; 
-export const getAuthHeaders = () => ({
-    'Content-Type': 'application/json',
-    'Authorization': `Bearer ${localStorage.getItem('token')}`,
-  }); 
