07cc47a9f0fe2986b9d0bc645f84a44e77cd5b9c
Bennacer Douirat <bennacer.douirat.solicode@gmail.com>
2025-07-15 00:25:51 +0100

    now ready to work on

--- social-project/backend/pkg/internal/services/utils/sendJSON_utils.go
+++
@@ -1,8 +0,0 @@
-package utils
-
-import "github.com/gorilla/websocket"
-
-func SendJSON(conn *websocket.Conn, messageType string, data map[string]any) error {
-	data["type"] = messageType
-	return conn.WriteJSON(data)
-}
